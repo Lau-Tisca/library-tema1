842120c362ab26abfdc73fcf12425faeda5e9b4d
Laurentiu Tisca <lau572004@gmail.com>
2025-07-15 16:02:38 +0300

    Initial commit

---
+++ library-main/src/main/java/com/practica/library/model/Book.java
@@ -0,0 +1,58 @@
+package com.practica.library.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class Book {
+
+    private Long id;
+    private String name;
+    private String author;
+
+    public Book(Long id, String name, String author) {
+        this.id = id;
+        this.author = author;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
